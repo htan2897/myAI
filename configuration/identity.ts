74ba1978c04f28e0557005a5ac7dbb62d8228527
Haoyu Tan <htan2897@gmail.com>
2025-02-19 19:08:36 -0500

    Update identity.ts
    changed the owner description and AI name

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Hao Tan`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `First year MBA student at UNC KFBS`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Yuki AI`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
