3f238d3e3d9ce6296edb62f865a162ac4d73fc0e
Haoyu Tan <htan2897@gmail.com>
2025-02-19 19:01:51 -0500

    changed header of the chatbot
    went from the [header] to "yuki"

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `Yuki AI`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Hao Tan @ UNC KFBS`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
