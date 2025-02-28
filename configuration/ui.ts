5c60d2830feec8e7fd619f4290ee435fd6a703a9
Haoyu Tan <htan2897@gmail.com>
2025-02-28 14:29:45 -0500

    Update ui.ts
    changed the header

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Yuki AI`;
+export const CHAT_HEADER: string = `Yuki(勇記) AI Japan Travel Planner`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Hao Tan @ UNC KFBS`;
 export const CLEAR_BUTTON_TEXT: string = `CLEAR`;
