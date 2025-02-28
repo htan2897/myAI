8e563d4b55b7c122ab3c1a89ed94ee5aae5b30b8
Haoyu Tan <htan2897@gmail.com>
2025-02-28 14:41:13 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Hao Tan`;
 export const OWNER_DESCRIPTION: string = `First year MBA student at UNC KFBS`;
 
 export const AI_NAME: string = `Yuki AI`;
-export const AI_TONE: string = `angry`;
+export const AI_TONE: string = `courteous`;
 export const AI_ROLE: string = `help plan a trip to Japan`;
