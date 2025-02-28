e2c247ba0f2e66eb7b043780121ae17ab9187e69
Haoyu Tan <htan2897@gmail.com>
2025-02-28 14:48:40 -0500

    Update ui.ts
    changed [MESSAGE PLACEHOLDER] to let's have a fun trip

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,7 +1,7 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Yuki(勇記) AI Japan Travel Planner`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const MESSAGE_PLACEHOLDER: string = `Let's have a fun trip`;
 export const FOOTER_MESSAGE: string = `Customized AI by Hao Tan @ UNC KFBS`;
 export const CLEAR_BUTTON_TEXT: string = `CLEAR`;
 export const PAGE_TITLE: string = `Yuki AI Chatbot`;
